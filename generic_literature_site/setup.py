a70ef47ad8fdc4cfa8225e0ccdf11c07cfe3ef95
Carsten Agger <carstena@magenta.dk>
2020-12-08 14:19:50 +0000

    Update version, release notes for v. 0.1.2

--- generic_literature_site/setup.py
+++ generic_literature_site/setup.py
@@ -40,7 +40,7 @@ requires = [
 
 setup(
     name="generic_literature_site",
-    version="0.1.1",
+    version="0.1.2",
     description="generic_literature_site",
     long_description=README + "\n\n" + CHANGES,
     classifiers=[
